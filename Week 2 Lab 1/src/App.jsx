c42f9a7f0a605d2d0357d4f8ec0e7edca4066425
wf-bcit <164838303+wf-bcit@users.noreply.github.com>
2024-09-24 20:06:35 -0700

    new stuff

---
+++ Week 2 Lab 1/src/App.jsx
@@ -0,0 +1,33 @@
+import { AlbumMain, AlbumSub } from "./Album";
+import { albums } from "./data";
+
+
+function App() {
+
+  var i=0;
+
+  return (
+    <div>
+
+      <h1 className="title-text">COMP3013 - Week 2 Lab 1</h1>
+      {
+        <AlbumMain
+          id={albums[i].id}
+          name={albums[i].name}
+          coverImg={albums[i].coverImg} 
+          tracks={albums[i].tracks}
+        />
+      }
+
+    <h2 className="select-text">Select an Album</h2>
+     {albums.map((album) => (
+        <AlbumSub
+          name={album.name}
+          coverImg={album.coverImg} 
+        />
+      ))}
+    </div>  
+  );
+}
+
+export default App
