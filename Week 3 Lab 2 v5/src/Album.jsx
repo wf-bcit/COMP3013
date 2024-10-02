22b7be8d16869399b83ef2de904f2d7120e61236
wf-bcit <164838303+wf-bcit@users.noreply.github.com>
2024-10-01 20:16:22 -0700

    udpate

---
+++ Week 3 Lab 2 v5/src/Album.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+export function AlbumMain({name, coverImg, tracks}){
+//Create a main section for an album and it's tracks
+    return (
+    <div className="albumMain-section">
+        <div>
+            <h2>{name}</h2>
+        </div>
+        <div className="albumMain-sub-section">
+            <img src={coverImg} alt="album cover big" />
+            <ol>{tracks.map((track) => (<li><span className="track">{track}<img src="https://clipart-library.com/images/8czn5zKKi.png" className="play-song-icon" alt="play song icon" /></span></li>))}</ol>
+        </div>
+    </div>
+    );
+}
+
+export function AlbumSub({id, name, coverImg}){
+//Create a sub section for an album name and cover
+    return (
+
+    <div className="albumSub-section">
+        <button id={id}><img src={coverImg} alt="album cover small" /></button>
+        <h3>{name}</h3>
+    </div>
+    );
+}
+
+
+export function AlbumList({albums}) 
+{
+  const [albumName, setAlbumName] = useState('');
+  const [coverURL, setCoverURL] = useState('');
+  const [listOfAlbums, setListOfAlbums] = useState(albums);
+
+  const albumNameEntry = (e) => {setAlbumName(e.target.value)};
+  const coverURLEntry = (e) => {setCoverURL(e.target.value)};
+  const albumAdd = () =>
+  {
+    let count = listOfAlbums.length;
+    setListOfAlbums( [ ...listOfAlbums, { id: count, name: albumName, coverImg: coverURL, tracks: ["Track1", "Track2", "Track3", "Track4"] } ] );
+    setAlbumName('');
+    setCoverURL(''); 
+  };
+
+  return (
+    <>
+      <h3>Add Album:</h3>
+      <input className="album-input" value={albumName} onChange={albumNameEntry} placeholder="Add Album"/>
+      <input className="album-input" value={coverURL} onChange={coverURLEntry} placeholder="Cover URL"/>
+      <button className="album-input" onClick={albumAdd}>ADD</button>
+      <ul>
+        {listOfAlbums.map(album => (
+        <AlbumSub
+          id={album.id}
+          name={album.name}
+          coverImg={album.coverImg} 
+        />
+      ))}
+      </ul>
+    </>
+  );
+}
---
+++ Week 3 Lab 2 v5/src/App.jsx
@@ -0,0 +1,29 @@
+import { AlbumMain, AlbumSub, AlbumList } from "./Album";
+import { albums } from "./data";
+
+
+function App() 
+{
+  let i=0;
+
+  return (
+    <div>
+
+    <h1 className="title-text">COMP3013 - Week 3 Lab 2</h1>
+    {
+      <AlbumMain
+        name={albums[i].name}
+        coverImg={albums[i].coverImg} 
+        tracks={albums[i].tracks}
+      />
+    }
+
+    <h2 className="select-text">Select an Album</h2>
+    <AlbumList albums={albums} />
+
+    </div>  
+  );
+}
+
+
+export default App
